399278c79296085fb22fdb3d8e4b6e5290ac1826
paul-shannon <paul.thurmond.shannon@gmail.com>
2021-11-18 17:50:17 -0800

    rudimentary working version, BACH1 + DDX3 example

---
+++ R/RnaBindingProtein.R
@@ -0,0 +1,141 @@
+#' @title RnaBindingProtein
+#' @description assess RNA-binding protein binding sites, their structure and motif matches
+#' @name RnaBindingProtein
+
+#' @import annotatr
+#' @import rtracklayer
+#' @import TxDb.Hsapiens.UCSC.hg38.knownGene
+#' @import igvR
+#'
+#' @field rbp the gene symbol name of the protein
+#'
+#' @examples
+#'   rbp <- RnaBindingProtein("DDX3X", "path/to/ENCFF565FNW.bigBed", "K562", "path/to/ddx3x.meme")
+#' @export
+#----------------------------------------------------------------------------------------------------
+RnaBindingProtein = R6Class("RnaBindingProtein",
+
+    #--------------------------------------------------------------------------------
+    private = list(rbp=NULL,
+                   genome=NULL,
+                   cellType=NULL,
+                   bigBedFile=NULL,       # likely from encode, eCLIP results
+                   tbl.rbpHits=NULL,
+                   motifs.meme.file=NULL,
+                   targetGene=NULL,
+                   utrAnnotations=NULL,
+                   tbl.3utr=NULL,
+                   tbl.5utr=NULL,
+                   igv=NULL,
+                   importBigBedFile=function(filename){
+                       gr <- import(filename)
+                       stopifnot("field9" %in% colnames(mcols(gr)))
+                       gr$score <- as.numeric(gr$field9)
+                       gr <- gr[gr$score > 1]
+                       tbl<- as.data.frame(gr)
+                       colnames(tbl)[1] <- "chrom"
+                       tbl$chrom <- as.character(tbl$chrom)
+                       invisible(tbl)
+                       } # importBigBedFile
+                   ),
+
+
+    #--------------------------------------------------------------------------------
+    public = list(
+         #' @description
+         #' Creates a new instance of this [R6][R6::R6Class] class.
+         #'
+         #' @param rpb character, gene symbol identifier for the rbp
+         #' @param targetGene character, gene symbol presumably having associated rbp binding sites
+         #' @param bigBedFile character, full path to the ENCODE file with binding sites
+         #' @param cellType character, typically "K562"
+         #' @param motifs.meme.file character, full path to an optional motifs file,
+         #'        calculated separately, describing motifs for the rbp
+        initialize = function(rbp, targetGene, bigBedFile, cellType, motifs.meme.file=NA){
+            private$rbp <- rbp
+            private$targetGene <- targetGene
+            private$genome <- "hg38"            # fixed for now
+            stopifnot(file.exists(bigBedFile))
+            private$bigBedFile <- bigBedFile
+            private$tbl.rbpHits <- private$importBigBedFile(bigBedFile)
+            private$cellType <- cellType
+            private$motifs.meme.file <- motifs.meme.file
+            annotations.file <- system.file(package="RnaBindingProtein", "extdata", "anno.hg38.utrs.RData")
+            stopifnot(file.exists(annotations.file))
+            private$utrAnnotations <- get(load(annotations.file))
+            },
+
+        #------------------------------------------------------------
+          #' @description
+          #' returns the slightly processed and filtered data.frame version of the ENCODE bigbed
+        getBindingTable = function(){
+           invisible(private$tbl.rbpHits)
+           },
+
+        #------------------------------------------------------------
+          #' @description
+          #' the bioc annotatr class offers a large set of genic annotations, ultimately
+          #' obtained from UCSC via TxDb.Hsapiens.UCSC.hg38.knownGene
+          #' return genic annotation types for hg38
+        getAnnotationTypes = function(){
+           grep("hg38", builtin_annotations(), v=TRUE)
+           },
+
+        #------------------------------------------------------------
+          #' @description
+          #' start, and retain a reference to, an igvR for the current genome and targetGene
+          #' return igv reference
+        start.igv = function(){
+           igv <- igvR()
+           setGenome(igv, private$genome)
+           showGenomicRegion(igv, private$targetGene)
+           setBrowserWindowTitle(igv, private$targetGene)
+           # setTrackHeight(igv, "Refseq Genes", 120)
+           zoomOut(igv)
+           private$igv <- igv
+           igv
+           },
+
+        #------------------------------------------------------------
+          #' @description
+          #' start, and retain a reference to, an igvR for the current genome and targetGene
+          #' return igv reference
+        showUTRs = function(){
+           genic.anno <- private$utrAnnotations[private$utrAnnotations$symbol %in% private$targetGene]
+           tbl.anno <- as.data.frame(genic.anno)
+           colnames(tbl.anno)[1] <- "chrom"
+           tbl.anno$chrom <- as.character(tbl.anno$chrom)
+           dups <- which(duplicated(tbl.anno[, c("start", "end", "type")]))
+           if(length(dups) > 0)
+               tbl.anno <- tbl.anno[-dups,]
+           tbl.3utr <- tbl.anno[grep("3UTR", tbl.anno$type),]
+           dim(tbl.3utr)
+           tbl.5utr <- tbl.anno[grep("5UTR", tbl.anno$type),]
+           dim(tbl.5utr)
+           tbl.track <- tbl.5utr[, c("chrom", "start", "end", "id")]
+           track <- DataFrameAnnotationTrack("5'UTR", tbl.track, color="brown", displayMode="Collapse")
+           displayTrack(private$igv, track)
+           tbl.track <- tbl.3utr[, c("chrom", "start", "end", "id")]
+           track <- DataFrameAnnotationTrack("3'UTR", tbl.track, color="black")
+           displayTrack(private$igv, track)
+           private$tbl.3utr  <- tbl.3utr
+           private$tbl.5utr  <- tbl.5utr
+           },
+
+        #------------------------------------------------------------
+          #' @description
+          #' retrieve the rbp bindings sites within the specified region
+          #' @param annotation.types  character, one or more annotatr categoies
+          #' @param roi list with chrom, start, end fiels
+          #' return data.frame
+        getBindingSites = function(annotation.types, roi){
+          result <- data.frame()
+          if(is.list(roi) & all(c("chrom", "start", "end") %in% names(roi))){
+             result <- subset(private$tbl.rbpHits, chrom==roi$chrom & start>=roi$start & end <= roi$end)
+             }
+           result
+           } # getBindingSites
+
+       ) # public
+    ) # class
+#--------------------------------------------------------------------------------
---
+++ inst/unitTests/test_RnaBindingProtein.R
@@ -0,0 +1,81 @@
+library(RUnit)
+library(RnaBindingProtein)
+#----------------------------------------------------------------------------------------------------
+runTests <- function()
+{
+    test_ctor()
+    test_getAnnotationTypes()
+    test_findBindingSites()
+
+} # runTests
+#----------------------------------------------------------------------------------------------------
+test_ctor <- function()
+{
+    message(sprintf("--- test_ctor"))
+
+    eclip.file <- system.file(package="RnaBindingProtein", "extdata", "ENCFF565FNW.bigBed")
+    checkTrue(file.exists(eclip.file))
+    rbp <- RnaBindingProtein$new("DDX3X", "BACH1", eclip.file, "K562")
+    checkTrue(all(c("R6", "RnaBindingProtein") %in% class(rbp)))
+
+    tbl.rbp <- rbp$getBindingTable()
+    checkEquals(dim(tbl.rbp), c(29606, 11))
+    score.stats <- fivenum(tbl.rbp$score) # 1.000774   1.422952   2.526644   6.016320 400.000000
+    checkEquals(score.stats[1], 1.0, tol=1)
+    checkEquals(score.stats[5], 400.0, tol=1)
+
+
+} # test_ctor
+#----------------------------------------------------------------------------------------------------
+test_getAnnotationTypes <- function()
+{
+    message(sprintf("--- test_getAnnotationTypes"))
+
+    eclip.file <- system.file(package="RnaBindingProtein", "extdata", "ENCFF565FNW.bigBed")
+    rbp <- RnaBindingProtein$new("DDX3X", "BACH1", eclip.file, "K562")
+
+    expected <- c("hg38_basicgenes", "hg38_cpg_inter", "hg38_cpg_islands", "hg38_cpg_shelves",
+                  "hg38_cpg_shores", "hg38_cpgs", "hg38_enhancers_fantom", "hg38_genes_1to5kb",
+                  "hg38_genes_3UTRs", "hg38_genes_5UTRs", "hg38_genes_cds",
+                  "hg38_genes_exonintronboundaries", "hg38_genes_exons", "hg38_genes_firstexons",
+                  "hg38_genes_intergenic", "hg38_genes_intronexonboundaries", "hg38_genes_introns",
+                  "hg38_genes_promoters", "hg38_lncrna_gencode")
+    checkEquals(sort(rbp$getAnnotationTypes()), expected)
+
+} # test_getAnnotationTypes
+#----------------------------------------------------------------------------------------------------
+test_start.igv <- function()
+{
+    if(!interactive()) return()
+
+    message(sprintf("--- test_start.igv"))
+
+    eclip.file <- system.file(package="RnaBindingProtein", "extdata", "ENCFF565FNW.bigBed")
+    rbp <- RnaBindingProtein$new("DDX3X", "BACH1", eclip.file, "K562")
+    igv <- rbp$start.igv()
+
+} # test_start.igv
+#----------------------------------------------------------------------------------------------------
+test_findBindingSites <- function(viz=FALSE)
+{
+    message(sprintf("--- test_findBindingSites"))
+
+    eclip.file <- system.file(package="RnaBindingProtein", "extdata", "ENCFF565FNW.bigBed")
+    rbp <- RnaBindingProtein$new("DDX3X", "BACH1", eclip.file, "K562")
+    toi <- c("hg38_genes_3UTRs", "hg38_genes_5UTRs")
+    checkEquals(sort(grep("UTR", rbp$getAnnotationTypes(), value=TRUE)), toi)
+    roi <- list(chrom="chr21", start=29274856, end=29392243)
+    tbl.bach1.hits <- rbp$getBindingSites(c("hg38_genes_3UTRs", "hg38_genes_5UTRs"), roi)
+    checkEquals(dim(tbl.bach1.hits), c(7, 11))
+
+    if(interactive() & viz){
+       igv <- rbp$start.igv()
+       #setTrackHeight(igv, "Refseq Genes", 100)
+       rbp$showUTRs()
+       }
+
+
+} # test_findBindingSites
+#----------------------------------------------------------------------------------------------------
+if(!interactive())
+    runTests()
